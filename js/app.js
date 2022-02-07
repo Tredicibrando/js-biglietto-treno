7a607f21b88aee69216fbc75388520c107df0a1c
Tredicibrando <nicolosigiov@gmail.com>
2022-02-07 15:20:05 +0100

    variabili_2

--- js/app.js
+++ js/app.js
@@ -1,4 +1,12 @@
 let userKm = parseFloat(prompt('inserisci il numero di km da effettuare'));
 let userAge = parseFloat(prompt ('inserisci qui la tua età')) ;
 
+const PRICE_KM = 0.21
+
 console.log( userKm , userAge )
+
+let sconto = 0;
+
+let prezzoFinale = userKm * PRICE_KM
+
+console.log(prezzoFinale)
